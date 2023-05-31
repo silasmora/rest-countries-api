8676f0a1f08b97c855d5e4bd3b5940557c2bd139
Silas Morales <silas.mora22@gmail.com>
2023-05-31 13:29:33 -0700

    added more routing

--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -8,12 +8,14 @@ export default function CountryCard({ filteredAndSearchCountries }) {
       {filteredAndSearchCountries.map(country => (
         <div key={country.name} className='shadow-lg bg-white rounded-md dark:bg-darkElements dark:text-darkModeText md:h-max xl:h-full'>
           <div className='h-1/2'>
-            <img className='h-full w-full rounded-t-md' src={country.flags.png} alt="country flag" />
+            <Link to={`/${country.name}`}>
+              <img className='h-full w-full cursor-pointer rounded-t-md' src={country.flags.png} alt="country flag" />
+            </Link>
           </div>
           <div className='p-6 border-t border-gray-100'>
 
             <Link to={`/${country.name}`}>
-              <h1 className='pb-4 text-xl font-bolder'>{country.name}</h1>
+              <h1 className='pb-4 text-xl font-bolder hover:text-red-600'>{country.name}</h1>
             </Link>
 
             <p><span className='font-bold'>Population: </span>{country.population.toLocaleString()}</p>
